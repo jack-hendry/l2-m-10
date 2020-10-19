298ced3a0dfadc72db12410bea9591de02c927c3
Jediael Mendoza <71054216+jediaelmendoza@users.noreply.github.com>
2020-10-19 15:06:15 -0400

    Adding team member D part
    Author: Jediael Mendoza

--- lab5/CD.py
+++ lab5/CD.py
@@ -1,5 +1,7 @@
 # SYSC3010 Lab 5 - CD.py
 #     The file to be jointly edited by Team Members C and D
+from D import * #Line added by Team member D
 
 def function_CD():
+    function_D() #line added by team member D
     print ("CD")
