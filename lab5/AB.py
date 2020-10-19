1cd51104204193a9df65b46d62a0585625c8ddbf
jackhendry881 <jackhendry@cmail.carleton.ca>
2020-10-19 17:03:52 -0500

    added lines for member B

--- lab5/AB.py
+++ lab5/AB.py
@@ -1,8 +1,9 @@
 # SYSC3010 Lab 5 - AB.py
 #     The file to be jointly edited by Team Members A and B
 from A import *
-
+from B import *
 
 def function_AB():
     print("AB")
     function_A()
+    function_B()
