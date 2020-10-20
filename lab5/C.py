0b46063d9b9b63527b4da0755c21c69bda29f0f9
Bailey Lyster <71287969+baileylyster@users.noreply.github.com>
2020-10-19 20:10:11 -0400

    Update C.py

--- lab5/C.py
+++ lab5/C.py
@@ -2,6 +2,6 @@
 #     The file to be jointly edited by Team Members C and D
 from D import * #Line added by Team member D
 
-def function_CD():
+def function_C():
     function_D() #line added by team member D
-    print ("CD")
+    print ("C")
