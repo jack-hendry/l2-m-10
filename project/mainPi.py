b05e291fea7a7a32885f60a1b476cde176337127
sbatterton <71090555+sbatterton@users.noreply.github.com>
2020-12-07 12:49:08 -0500

    Update mainPi.py

--- project/mainPi.py
+++ project/mainPi.py
@@ -4,6 +4,9 @@ import http.client
 import time
 import sqlite3
 from datetime import datetime, date
+import smtplib
+from tkinter import *
+from tkinter.ttk import Combobox
 
 portionSize = 500
 waterBowlSize = 1000
@@ -14,6 +17,7 @@ foodStorageValue = 0
 foodBowlValue = 0
 waterStorageValue = 0
 waterBowlValue = 0
+email = ""
 
 dbconnect = sqlite3.connect("/home/pi/Documents/SYSC 3010/Labs/Project/projectdb.db")
 cursor = dbconnect.cursor()
@@ -75,11 +79,24 @@ def waitResponse():
     broken = 0
     while True:
         broken += 1
-        if broken == 30:
+        if broken == 60:
             return False
         if getMessage()["entry_id"] == (currentID + 1):
             return True
         time.sleep(0.1)
+
+
+def sendMail(subject, body, email):
+    GMAIL_USERNAME = "FitPet3010@gmail.com"  # change this to match your gmail account
+    GMAIL_PASSWORD = "fitpet2020!!"
+    with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(GMAIL_USERNAME, GMAIL_PASSWORD)
+        msg = f"Subject: {subject}\n\n{body}"
+        smtp.sendmail(GMAIL_USERNAME, email, msg)
+
 
 def checkFillFood():
     foodStorageValue = 0
@@ -98,10 +115,10 @@ def checkFillFood():
         print("failed")
         exit()
 
-    if foodStorageValue < 1:
-        print("food storage empty")
-    elif foodBowlValue > 100000000:
-        print("food bowl full")
+    if foodStorageValue > 16:
+        sendMail(
+            "FitPet Notice", "The food storage is empty in your FitPet system.", email
+        )
     else:
         time.sleep(1)
         currentID = getMessage()["entry_id"]
@@ -109,13 +126,13 @@ def checkFillFood():
         if waitResponse:
             print("done")
             sql = (
-            "INSERT INTO tblFoodLog (amountBefore, amountFilled, time) values("
-            + str(foodBowlValue)
-            + ", "
-            + str(foodToFill)
-            + ", '"
-            + str(datetime.now())
-            + "');"
+                "INSERT INTO tblFoodLog (amountBefore, amountFilled, time) values("
+                + str(foodBowlValue)
+                + ", "
+                + str(foodToFill)
+                + ", "
+                + str(datetime.now())
+                + ");"
             )
             cursor.execute(sql)
             dbconnect.commit()
@@ -139,9 +156,9 @@ def checkFillWater():
         exit()
 
     if waterStorageValue < 1:
-        print("water storage empty")
-    elif waterBowlValue > 1000000000:
-        print("water bowl full")
+        sendMail(
+            "FitPet Notice", "The water storage is empty in your FitPet system.", email
+        )
     else:
         time.sleep(1)
         currentID = getMessage()["entry_id"]
@@ -149,13 +166,13 @@ def checkFillWater():
         if waitResponse:
             print("done")
             sql = (
-            "INSERT INTO tblWaterLog (amountBefore, amountFilled, time) values("
-            + str(waterBowlValue)
-            + ", "
-            + str(waterToFill)
-            + ", '"
-            + str(datetime.now())
-            + "');"
+                "INSERT INTO tblWaterLog (amountBefore, amountFilled, time) values("
+                + str(waterBowlValue)
+                + ", "
+                + str(waterToFill)
+                + ", "
+                + str(datetime.now())
+                + ");"
             )
             cursor.execute(sql)
             dbconnect.commit()
@@ -163,13 +180,101 @@ def checkFillWater():
         else:
             print("failed")
             sql = (
-            "INSERT INTO tblWaterLog (amountBefore, amountFilled, time) values("
-            + str(waterBowlValue)
-            + ", "
-            + str(waterToFill)
-            + ", '"
-            + str(datetime.now())
-            + "');"
+                "INSERT INTO tblWaterLog (amountBefore, amountFilled, time) values("
+                + str(waterBowlValue)
+                + ", "
+                + str(waterToFill)
+                + ", "
+                + str(datetime.now())
+                + ");"
             )
             cursor.execute(sql)
             dbconnect.commit()
+
+class MyWindow:
+    def __init__(self, win):
+        self.lbl5 = Label(win, text = "Type of pet:")
+        self.lbl5.place(x=0, y=0)
+        self.v1= StringVar()
+        self.v1.set(1)
+        self.catRB = Radiobutton(win, text="Cat", variable=self.v1,value="Cat")
+        self.dogRB = Radiobutton(win, text="Dog", variable=self.v1,value= "Dog")
+        self.catRB.place(x=150, y=0)
+        self.dogRB.place(x=200, y=0)
+        self.lbl6 = Label(win, text = "Sex of pet: ")
+        self.lbl6.place(x=0, y=50)
+        self.v4 = StringVar() 
+        self.v4.set(1)
+        self.maleRB = Radiobutton(win, text="Male ", variable=self.v1,value="Male")
+        self.femaleRB = Radiobutton(win, text="Female ", variable=self.v1,value="Female")
+        self.maleRB.place(x=150, y=50)
+        self.femaleRB.place(x=200, y=50)
+        self.lbl4 = Label(win, text = "Size of Breed:")
+        self.lbl4.place(x=0, y=100)
+        self.v2= StringVar()
+        self.v2.set(1)
+        self.smallBreedRB = Radiobutton(win, text="Small", variable=self.v2,value="Small")
+        self.mediumBreedRB = Radiobutton(win, text="Medium", variable=self.v2,value= "Medium")
+        self.largeBreedRB = Radiobutton(win, text="Large", variable=self.v2,value= "Medium")
+        self.smallBreedRB.place(x=150, y=100)
+        self.mediumBreedRB.place(x=210, y=100)
+        self.largeBreedRB.place(x= 285, y=100)
+        self.lbl1=Label(win, text='Pets Name: ')
+        self.lbl1.place(x=0, y=150)
+        self.t1=Entry(bd=3)
+        self.t1.place(x=150, y=150)
+        self.lbl2=Label(win, text='Pet Owners Email address:')
+        self.lbl2.place(x=0, y=200)
+        self.t2=Entry()
+        self.t2.place(x=150, y=200)
+        self.foodAmount =Label(win, text='Daily Amount of Food:')
+        self.foodAmount.place(x=0, y=250)
+        self.t4=Entry()
+        self.t4.place(x=150, y=250)
+        self.lbl3=Label(win, text='Number of meals/day: ')
+        self.lbl3.place(x=0, y=300)
+        self.v0=IntVar()
+        self.v0.set(1)
+        self.r1 = Radiobutton(win, text="2", variable=self.v0,value=2)
+        self.r2 = Radiobutton(win, text="3", variable=self.v0,value=3)
+        self.r3 = Radiobutton(win, text="4", variable=self.v0,value=4)
+        self.r4 = Radiobutton(win, text="5", variable=self.v0,value=5)
+        self.r1.place(x=150, y=300)
+        self.r2.place(x=200,y=300)
+        self.r3.place(x=250,y=300)
+        self.r4.place(x=300,y=300)
+        self.b1=Button(win, text='Submit', command = self.submit)
+        self.b1.place(x=150, y=350)
+
+    def submit(self): # input 2 variables 
+        typePet = self.v1.get()
+        breedSize = self.v2.get()
+        petName = self.t1.get()
+        ownerEmail = self.t2.get()
+        setNumberMeals =int( self.v0.get() )
+        sexOfPet = self.v4.get()
+        dailyFood = int(self.t4.get()) 
+        #testing purposes 
+        print( typePet + " " + sexOfPet +"  " + breedSize + " " + petName +"  " + ownerEmail + " "
+        + str(dailyFood) + " " + str(setNumberMeals)  )
+
+
+def baileyTest():
+    currentID = getMessage()["entry_id"]
+    sendMessage(1, None, 50)
+    print(waitResponse())
+
+    currentID = getMessage()["entry_id"]
+    sendMessage(1, 35, None)
+    print(waitResponse())
+
+window=Tk()
+mywin=MyWindow(window)
+window.title('Hello Python')
+window.geometry("500x400+10+10")
+window.mainloop()
+
+baileyTest()
+
+while True:
+    exit()
